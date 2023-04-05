0507199dbae57abd8017c745319fc5d464aa9b34
João Luís Cardoso <103074041+JoaoLuisC@users.noreply.github.com>
2023-04-04 23:12:31 -0300

    Add files via upload

---
+++ Revisao_prova/JoaoCardoso_04-04-2023/src/joaocardoso_04/pkg04/pkg2023/Paciente.java
@@ -0,0 +1,45 @@
+package joaocardoso_04.pkg04.pkg2023;
+
+
+public class Paciente {
+     private String nome, cpf, enfermidade;
+    private int codPaciente;
+    private static int qtdPacientes;
+
+    Paciente(){
+        Paciente.qtdPacientes = Paciente.qtdPacientes + 1;
+        this.codPaciente = qtdPacientes;
+    }
+    
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public void setEnfermidade(String enfermidade) {
+        this.enfermidade = enfermidade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public String getEnfermidade() {
+        return enfermidade;
+    }
+
+    public int getCodaciente() {
+        return codPaciente;
+    }
+
+    public static int getQtdPacientes() {
+        return qtdPacientes;
+    } 
+}
